dd0dbf36887ecc6e4757150facfc80313e33510e
ElvisG <145528838+ElvisGrigorjevs@users.noreply.github.com>
2024-02-11 14:51:20 +0200

    Added - Go, Search(W.I.P.), Forward and Back

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,45 @@ namespace WPFChromiumBrowser
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        ChromiumWebBrowser chrome;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ChromiumWebBrowser_OnFrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            Dispatcher.BeginInvoke((Action)(() =>
+            {
+                buttonForward.IsEnabled = Browser_Test.CanGoForward;
+                buttonBack.IsEnabled = Browser_Test.CanGoBack;
+            }));
+        }
+
+        private void buttonForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (Browser_Test.CanGoForward)
+            {
+                Browser_Test.Forward();
+            }
+        }
+
+        private void buttonBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (Browser_Test.CanGoBack)
+            {
+                Browser_Test.Back();
+            }
+        }
+
+        private void buttonGoTo_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(boxSearch.Text))
+            {
+                Browser_Test.Address = boxSearch.Text;
+            }
         }
     }
 }
