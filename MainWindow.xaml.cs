07f76ba97cc625549cc212816b0cc8f89eb7fe63
ElvisG <145528838+ElvisGrigorjevs@users.noreply.github.com>
2024-02-19 19:10:14 +0200

    Revert "Removed unnecassary lines of code(Unused parts of code with "using" + one line I forgot to remove while learning/testing)"
    This reverts commit f136eb1111fe18c9d67727532c4dee7d7688c46d.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,20 @@
 ﻿using CefSharp;
 using CefSharp.Wpf;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace WPFChromiumBrowser
 {
@@ -11,6 +23,9 @@ namespace WPFChromiumBrowser
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        ChromiumWebBrowser chrome;
+
         public MainWindow()
         {
             InitializeComponent();
